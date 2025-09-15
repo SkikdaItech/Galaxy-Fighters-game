b7d77310b6e864cc430775dbf5088ba8eaca8dfa
Hani <145288127+elbos12@users.noreply.github.com>
2025-09-15 04:17:52 -1100

    .

--- main.py
+++ main.py
@@ -16,3 +16,17 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 # set capption of the game 
 pygame.display.set_caption("Galaxy Fighters game Game")
 
+
+#main function 
+def main():
+    run  = True
+    while run:
+        for event in pygame.event.get():
+            if event.type  == pygame.QUIT:
+                run = False
+        
+    pygame.quit()
+    
+    
+if __name__ == "__main__":
+    main()
