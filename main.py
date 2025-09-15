6a6c87b41b7c9938c5127de590b640b081f64b5d
Hani <145288127+elbos12@users.noreply.github.com>
2025-09-15 03:49:00 -1100

    setup the game

---
+++ main.py
@@ -0,0 +1,18 @@
+#import nesssasery libraries
+import pygame  
+import os
+
+# Initialize Pygame
+pygame.init()
+
+#setup the game window
+WIDTH = 800
+HEIGHT = 400
+
+#display the game
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+
+# set capption of the game 
+pygame.display.set_caption("Galaxy Fighters game Game")
+
